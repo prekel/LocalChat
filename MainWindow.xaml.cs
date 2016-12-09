e18e066ae8a57a1f8caa5a955b2f73e722be789b
Владислав Прекель <79232809414@yandex.ru>
2016-12-09 22:15:16 +0700

    добавлена отправка

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace LocalChat
 {
@@ -25,6 +26,44 @@ namespace LocalChat
 		public MainWindow()
 		{
 			InitializeComponent();
+			SendButton.Click += Send_Click;
+		}
+
+
+		public void ThreadSend(object Message)
+		{
+			try
+			{
+				var Msg = (string[])Message;
+				var MessageText = Msg[0];
+				var ip = Msg[1];
+
+				var EndPoint = new IPEndPoint(IPAddress.Parse(ip), 7000);
+				var Connector = new Socket(EndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				Connector.Connect(EndPoint);
+				var SendBytes = Encoding.Default.GetBytes(MessageText);
+				Connector.Send(SendBytes);
+				Connector.Close();
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+		public void SendMsg(string Text, RichTextBox Rtb)
+		{
+			Action action = () =>
+			{
+				var tr = new TextRange(Rtb.Document.ContentStart, Rtb.Document.ContentEnd);
+				tr.Text += Text;
+			};
+			Dispatcher.Invoke(action);
+		}
+
+		private void Send_Click(object sender, RoutedEventArgs e)
+		{
+			new Thread(new ParameterizedThreadStart(ThreadSend)).Start(new string[] { Message.Text, IP.Text });
 		}
 	}
 }
